00e55276c11a27b5ffc8f458c01f462d08f12dc0
unknown <antoniobatistajr@gmail.com>
2021-06-27 00:47:16 -0300

    Limpando formulario na troca de opção do calculo

--- calculadora/js/show_hide.js
+++ calculadora/js/show_hide.js
@@ -1,9 +1,15 @@
 function valoresCalc(){
+    $("#calculadora").each(function(){
+        this.reset();
+    });
     $("#valoresCalc").show();
     $("#valorTab").hide();
 }
 
 function valoresTabuada(){
+    $("#calculadora").each(function(){
+        this.reset();
+    });
     $("#valoresCalc").hide();
     $("#valorTab").show();
 }
